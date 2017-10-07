d9b9eeb77e09036973e5878f2d128cbb2392dc9b
Tyler Brown <tbrown9619@gmail.com>
2017-10-07 12:43:40 -0500

    got a basic timer running on the home page. is formatted by a computed property to show up as hours minutes and seconds

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,27 @@
 <template>
     <div class="hello">
-        <h1>{{ msg }}</h1>
-        <h2>Essential Links</h2>
-        <ul>
-            <li>
-                <a href="https://vuejs.org" target="_blank">Core Docs</a>
-            </li>
-            <li>
-                <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-            </li>
-            <li>
-                <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-            </li>
-            <li>
-                <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-            </li>
-            <br>
-            <li>
-                <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
-            </li>
-        </ul>
-        <h2>Ecosystem</h2>
-        <ul>
-            <li>
-                <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-            </li>
-            <li>
-                <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-            </li>
-            <li>
-                <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-            </li>
-            <li>
-                <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-            </li>
-        </ul>
+        <h2>Time since entering site</h2>
+        <span id="site-timer">{{ formatedSeonds }}</span>
+        <br>
     </div>
 </template>
 
 <script>
+const moment = require('moment');
 export default {
-    name: 'HelloWorld',
+    computed: {
+        formatedSeonds() {
+            return moment.utc(this.now * 1000).format('HH:mm:ss');
+        }
+    },
+    created() {
+        window.setInterval(() => {
+            this.now += 1;
+        }, 10);
+    },
     data() {
         return {
-            msg: 'Welcome to Your Vue.js App'
+            now: 0
         };
     }
 };
@@ -56,17 +34,7 @@ h2 {
     font-weight: normal;
 }
 
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
+#site-timer{
+    font-size: 4rem;
 }
 </style>
