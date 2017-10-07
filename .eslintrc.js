fc22825c7df4abde0f956b6becc2a3ae17e47e25
Tyler Brown <tbrown9619@gmail.com>
2017-10-07 12:00:29 -0500

    commiting vue template

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // No space before func paren
+    "space-before-function-paren": 0,
+    // require semi colons
+    "semi": [2, "always"],
+    // make the indent size 4
+    "indent": ["error", 4]
+  }
+}
